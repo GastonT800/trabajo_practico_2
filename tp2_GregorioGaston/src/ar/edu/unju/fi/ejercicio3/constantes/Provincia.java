56f9e23745084b9c8694f14cb596f3a209017951
GastonT800 <m.nick5164@gmail.com>
2024-04-30 01:33:26 -0300

    se crearon los package del ejercicio3

---
+++ tp2_GregorioGaston/src/ar/edu/unju/fi/ejercicio3/constantes/Provincia.java
@@ -0,0 +1,11 @@
+package ar.edu.unju.fi.ejercicio3.constantes;
+
+public enum Provincia {
+	
+	JUJUY, SALTA, TUCUMAN, CATAMARCA, LARIOJA, SANTIAGODELESTERO;
+
+	//Atributos
+	private int cantidPoblacion;
+	private double superficie;
+	
+}
