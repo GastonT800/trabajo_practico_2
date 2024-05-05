f5c2383ad4a74e1ba638984f4c66b1021eb4cdee
GastonT800 <m.nick5164@gmail.com>
2024-05-04 22:39:06 -0300

    se finalizo el ejercicio3

--- tp2_GregorioGaston/src/ar/edu/unju/fi/ejercicio3/constantes/Provincia.java
+++ tp2_GregorioGaston/src/ar/edu/unju/fi/ejercicio3/constantes/Provincia.java
@@ -2,10 +2,45 @@ package ar.edu.unju.fi.ejercicio3.constantes;
 
 public enum Provincia {
 	
-	JUJUY, SALTA, TUCUMAN, CATAMARCA, LARIOJA, SANTIAGODELESTERO;
+	JUJUY(811611, 53244.2),
+	SALTA(1441351, 155488),
+	TUCUMAN(1731820, 22524),
+	CATAMARCA(415438, 101486.1),
+	LA_RIOJA(393531, 91493.7),
+	SANTIAGO_DEL_ESTERO(1060906, 136351);
 
 	//Atributos
-	private int cantidPoblacion;
+	private int cantidadPoblacion;
 	private double superficie;
+	
+	//Constructor
+	private Provincia(int cantidadPoblacion, double superficie) {
+		this.cantidadPoblacion = cantidadPoblacion;
+		this.superficie = superficie;
+	}
+
+	//Getters and Setters
+	public int getCantidadPoblacion() {
+		return cantidadPoblacion;
+	}
+
+	public void setCantidadPoblacion(int cantidadPoblacion) {
+		this.cantidadPoblacion = cantidadPoblacion;
+	}
+
+	public double getSuperficie() {
+		return superficie;
+	}
+
+	public void setSuperficie(double superficie) {
+		this.superficie = superficie;
+	}
+	
+	//Metodo
+	public double calcularDensidadPoblacional() {
+		double densidadPoblacional;
+		densidadPoblacional = this.cantidadPoblacion/this.superficie;
+		return densidadPoblacional;
+	}
 	
 }
--- tp2_GregorioGaston/src/ar/edu/unju/fi/ejercicio3/main/Main.java
+++ tp2_GregorioGaston/src/ar/edu/unju/fi/ejercicio3/main/Main.java
@@ -1,9 +1,22 @@
 package ar.edu.unju.fi.ejercicio3.main;
 
+import ar.edu.unju.fi.ejercicio3.constantes.Provincia;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Provincia[] provincias = Provincia.values();
+	
+		for(Provincia provincia: provincias) {
+			
+			System.out.println("\nProvincia - " + provincia);
+			System.out.println("Cantidad Poblacional: " +  provincia.getCantidadPoblacion() + " habitantes");
+			System.out.println("Superficie: " +  provincia.getSuperficie() + " km2");
+			System.out.println("Densidad Poblacional: " + provincia.calcularDensidadPoblacional() + " hab/km2");
+			
+		}
+
 		
 	}
 
