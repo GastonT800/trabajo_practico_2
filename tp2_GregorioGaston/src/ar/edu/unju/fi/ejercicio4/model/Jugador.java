60f63d53b34044a6b67abe6523818953e3f4f5ab
GastonT800 <m.nick5164@gmail.com>
2024-05-04 22:57:49 -0300

    se crearon los package para el ejercicio4

---
+++ tp2_GregorioGaston/src/ar/edu/unju/fi/ejercicio4/model/Jugador.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.ejercicio4.model;
+
+import java.util.Date;
+
+public class Jugador {
+	
+	//Atributos
+	private String nombre;
+	private String apellido;
+	private Date fechaNacimiento;
+	private String nacionalidad;
+	private double estatura;
+	private double peso;
+	
+}
