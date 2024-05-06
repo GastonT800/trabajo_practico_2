fe08ead11dd27377e96f3bb709713726630f7ffd
GastonT800 <m.nick5164@gmail.com>
2024-05-06 04:02:02 -0300

    se termino con el ejercicio4

--- tp2_GregorioGaston/src/ar/edu/unju/fi/ejercicio4/main/Main.java
+++ tp2_GregorioGaston/src/ar/edu/unju/fi/ejercicio4/main/Main.java
@@ -1,10 +1,183 @@
 package ar.edu.unju.fi.ejercicio4.main;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio4.constantes.Posicion;
+import ar.edu.unju.fi.ejercicio4.model.Jugador;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Scanner escaner = new Scanner(System.in);
 
+		ArrayList<Jugador> jugadores = new ArrayList<>();
+		
+		int opcion;
+		try {
+		do {
+			System.out.println("\n****************************");
+			System.out.println("********* MENU *************");
+			System.out.println("1- Alta de jugador");
+			System.out.println("2- Mostrar todos los jugadores");
+			System.out.println("3- Modificar la posición de un jugador");
+			System.out.println("4- Eliminar un jugador");
+			System.out.println("5- Salir");
+			System.out.print("\nSeleccione una opcion: ");
+			opcion = escaner.nextInt();
+			escaner.nextLine();
+			
+			 switch (opcion) {
+             case 1:
+            	 System.out.println("\n*** Alta de jugador ***");
+            	 System.out.print("Ingrese nombre del jugador: ");
+                 String nombre = escaner.nextLine();
+                 System.out.print("Ingrese apellido del jugador: ");
+                 String apellido = escaner.nextLine();
+                 System.out.println("Acontinuacion ingresera su fecha de nacimiento en orden");
+                 System.out.print("Ingrese su dia: ");
+                 int dia = escaner.nextInt();
+                 System.out.print("Ingrese el mes: ");
+                 int mes = escaner.nextInt();
+                 System.out.print("Ingrese el año: ");
+                 int anio = escaner.nextInt();
+                 LocalDate fechaNacimiento = LocalDate.of(anio,mes,dia);
+                 escaner.nextLine();
+                 System.out.println("La fecha de nacimiento ingresada fue: " + dia + "/" + mes + "/" + anio);
+                 System.out.print("Ingrese la nacionalidad: ");
+                 String nacionalidad = escaner.nextLine();
+                 System.out.print("Ingrese la estatura: ");
+                 double estatura = escaner.nextDouble();
+                 System.out.print("Ingrese el peso: ");
+                 double peso = escaner.nextDouble();
+                 System.out.println("\n*** Posiciones ***");
+                 int i=0;
+                 for(Posicion posicion: Posicion.values()) {
+                	 i+=1;
+                	 System.out.println(i+" - " + posicion);
+                 }
+                 System.out.print("\nIngrese la posicion del jugador: ");
+                 int pos = escaner.nextInt();
+                 while(pos<1 || pos>4) {
+                	 System.out.print("Ingrese la posicion del jugador: ");
+                	 pos = escaner.nextInt();
+                 }
+                 escaner.nextLine();
+                 Posicion posicion = Posicion.values()[pos - 1];
+                 //System.out.println("La posicion del jugadpr es: " + posicion);
+                 
+                 Jugador nuevoJudador = new Jugador(nombre, apellido, fechaNacimiento, nacionalidad, estatura, peso, posicion);
+                 jugadores.add(nuevoJudador);
+                 System.out.println("\nEl alta del jugador fue cargado correctamente");
+                 break;
+             case 2:
+            	 if(jugadores.isEmpty()) {
+ 					
+ 					System.out.println("\nPara mostrar los jugadores primero tiene que dar de alta un jugador");
+ 					
+ 				}else {
+            	 System.out.println("\n*** Lista de jugadores ***");
+                 for (Jugador jugador : jugadores) {
+                     System.out.println(jugador);
+                 }
+ 				}
+                 break;
+             case 3:
+            	 
+            	if(jugadores.isEmpty()) {
+  					
+  					System.out.println("\nPara modificar la posicíon de un jugador primero tiene que dar de alta un jugador");
+  					
+  				}else {
+            	 
+	            	 System.out.println("\n*** Modificar la posición de un jugador ***");
+	            	 System.out.print("Ingrese el nombre del jugador: ");
+	            	 nombre = escaner.nextLine();
+	            	 System.out.print("Ingrese el apellido del jugador: ");
+	            	 apellido = escaner.nextLine();
+	            	 boolean buscar = false;
+	            	 for (Jugador jugador : jugadores) {
+	            	        if (jugador.getNombre().equalsIgnoreCase(nombre) && jugador.getApellido().equalsIgnoreCase(apellido)) {
+	            	            System.out.println(jugador);
+	
+	            	            // Solicitar la nueva posición
+	            	            System.out.println("\n*** Posiciones ***");
+	            	            int j = 0;
+	            	            for (Posicion posic:Posicion.values()) {
+	            	                j++;
+	            	                System.out.println(j + " - " + posic);
+	            	            }
+	            	            System.out.print("\nIngrese la nueva posición del jugador: ");
+	            	            int nuevaPos = escaner.nextInt();
+	            	            while (nuevaPos < 1 || nuevaPos > 4) {
+	            	                System.out.print("Ingrese la nueva posición del jugador: ");
+	            	                nuevaPos = escaner.nextInt();
+	            	            }
+	            	            escaner.nextLine();
+	            	            Posicion nuevaPosicion = Posicion.values()[nuevaPos - 1];
+	            	            jugador.setPosicion(nuevaPosicion);
+	            	            System.out.println("\nLa posición del jugador fue modificada");
+	            	            buscar = true;
+	                            break;
+	            	            
+	            	        }
+	            	 }
+	            	 if (!buscar) {
+	                     System.out.println("\nNo se encontró a ningun jugador con ese nombre y apellido");
+	                 }
+  				 }
+            	
+                 break;
+             case 4:
+            	 
+            	 if(jugadores.isEmpty()) {
+  					
+  					System.out.println("\nPara eliminar un jugador primero tiene que dar de alta un jugador");
+  					
+  				}else {
+	            	 System.out.println("\n*** Eliminar jugador ***");
+	            	 System.out.print("Ingrese el nombre del jugador: ");
+	            	 nombre = escaner.nextLine();
+	            	 System.out.print("Ingrese el apellido del jugador: ");
+	            	 apellido = escaner.nextLine();
+	            	 
+	            	 Iterator<Jugador> iterador = jugadores.iterator();
+	            	 while (iterador.hasNext()) {
+	            		    Jugador jugador = iterador.next();
+	            		    if (jugador.getNombre().equalsIgnoreCase(nombre) && jugador.getApellido().equalsIgnoreCase(apellido)) {
+	            		        iterador.remove();
+	            		        System.out.println("\nJugador eliminado");
+	            		    }
+	            	 }
+	            	 
+	            	 if (!iterador.hasNext()) {
+	            	        System.out.println("\nNo se encontró a ningun jugador con ese nombre y apellido");
+	            	    }
+	
+  				 }
+            	 
+                 break;
+             case 5:
+                 System.out.println("\n****** FIN DEL PROGRAMA *******");
+                 break;
+             default:
+                 System.out.println("\nOPCION INCORRECTA");
+                 break;
+         }
+		
+		}while (opcion != 5);
+		
+		escaner.close();
+		
+		}catch(Exception e) {
+			
+	        System.out.println("Se ha Producido un Error durante la Ejecutacion del Programa");
+	        System.out.println("****** PROGRAMA FINALIZADO ****");
+		}
+		
 	}
 
 }
--- tp2_GregorioGaston/src/ar/edu/unju/fi/ejercicio4/model/Jugador.java
+++ tp2_GregorioGaston/src/ar/edu/unju/fi/ejercicio4/model/Jugador.java
@@ -1,15 +1,114 @@
 package ar.edu.unju.fi.ejercicio4.model;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import ar.edu.unju.fi.ejercicio4.constantes.Posicion;
 
 public class Jugador {
 	
 	//Atributos
 	private String nombre;
 	private String apellido;
-	private Date fechaNacimiento;
+	private LocalDate fechaNacimiento;
 	private String nacionalidad;
 	private double estatura;
 	private double peso;
+	private Posicion posicion;
+	
+	//Constructor por defecto
+	public Jugador() {
+		super();
+	}
+	
+	//Constructor Parametrizado
+	public Jugador(String nombre, String apellido, LocalDate fechaNacimiento, String nacionalidad, double estatura,
+			double peso, Posicion posicion) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.fechaNacimiento = fechaNacimiento;
+		this.nacionalidad = nacionalidad;
+		this.estatura = estatura;
+		this.peso = peso;
+		this.posicion = posicion;
+	}
+
+	//Getters and Setters
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	public double getEstatura() {
+		return estatura;
+	}
+
+	public void setEstatura(double estatura) {
+		this.estatura = estatura;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	public void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Jugador nombre= " + nombre + " - apellido= " + apellido + " - fechaNacimiento= " + fechaNacimiento.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+				+ "- edad = " + calcularEdad() + " años - nacionalidad= " + nacionalidad + " - estatura= " + estatura + "cm - peso= " + peso + "kg - posicion= "
+				+ posicion;
+	}
+
+	//Metodo
+	public int calcularEdad() {
+		int edad=0;
+		
+		LocalDate fechaActual = LocalDate.now();
+		if(fechaActual.isAfter(fechaNacimiento)) {
+			edad = fechaActual.getYear()-fechaNacimiento.getYear();
+		}
+		return edad;
+	}
 	
 }
